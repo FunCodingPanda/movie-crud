67dc4512925afd25593ca0bc960679a68cd0884f
Sophie <sophie4ngo@gmail.com>
2018-04-21 22:28:19 -0700

    Add production database configuration

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,16 @@
 
 module.exports = {
   development: { client: 'pg', connection: { database: 'movie_crud' } },
-  test: { client: 'pg', connection: { database: 'movie_crud' } }
+  test: { client: 'pg', connection: { database: 'movie_crud' } },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
